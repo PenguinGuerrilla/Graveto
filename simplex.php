2e2248ec3eac5ee1c8c32de3c1581e709bbe363d
PenguinGuerrilla <uni.hsilva@gmail.com>
2024-05-08 18:58:41 -0300

    progresso na funcao escalonar

--- simplex.php
+++ simplex.php
@@ -1,11 +1,12 @@
 <?php
 $it = 0;
 $matriz = array(
-    array(-3,2,0,0,0,0),
+    array(3,5,0,0,0,0),
     array(1,0,1,0,0,4),
     array(0,2,0,1,0,12),
     array(3,2,0,0,1,18)
 );
+
 function printMat($mat){
     $lin = count($mat);
     $col = count($mat[0]);
@@ -16,7 +17,58 @@ function printMat($mat){
       echo "</br>";
     }
 }
+function escalona($mat,$pivot){
+  $div = $mat[$pivot[0]][$pivot[1]];
+  for($i = 0; $i < count($mat[0]); $i++){
+    $mat[$pivot[0]][$i] = ($mat[$pivot[0]][$i])/$div;
+  }
+  printMat($mat);
+  echo '</br>';
+  for($i = 0; $i < count($mat); $i++){
+    if($i != $pivot[0]){
+      for($j = 0; $j < count($mat[0]); $j++){
+        $mat[$i][$j] = $mat[$i][$j] - ($mat[$i][$j] * $mat[$pivot[0]][$pivot[$j]]);
+      }
+    }
+  }
+  return $mat;
+}
+function findPivot($mat){
+  $lin = count($mat);
+  $col = count($mat[0]);
+  $maxCol = -9999;
+  $pivotCol = 0;
+  $maxRow = 9999;
+  $pivotRow = 0;
 
+  for($j = 0; $j < $col; $j++){
+    if(abs($mat[0][$j]) > $maxCol){
+      $maxCol = $mat[0][$j];
+      $pivotCol = $j;
+    }
+  }
+  echo "Coluna pivo -> ".$pivotCol."<br>";
+
+  for($i = 1; $i < $lin; $i++){
+    if($mat[$i][$pivotCol] != 0){
+      echo "[".$i."][".($col-1)."] / [".$i."][".$pivotCol."] = ".$mat[$i][$col-1]/$mat[$i][$pivotCol]." </br>";
+      if(($mat[$i][$col-1]/$mat[$i][$pivotCol]) < $maxRow){
+        $maxRow = ($mat[$i][$col-1]/$mat[$i][$pivotCol]);
+
+        $pivotRow = $i;
+      }
+    }
+    
+  }
+  echo "Linha pivo -> ".$pivotRow."<br>";
+
+  echo "pivo = [".$pivotRow."][".$pivotCol."] -> ".$mat[$pivotRow][$pivotCol]."<br>";
+  echo '</br>';
+  $pivot = array($pivotRow,$pivotCol);
+  return $pivot;
+}
 printMat($matriz);
-
+$piv = findPivot($matriz);
+$m = escalona($matriz,$piv);
+printMat($m); 
 
